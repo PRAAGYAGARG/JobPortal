771015d98ac82fc1ed9175613ac03cade1ed73e4
Praagya <praagyagarg@gmail.com>
2025-08-06 16:10:58 +0530

    first commit

---
+++ backend/models/job.model.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+const jobSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    requirements: [{
+        type: String
+    }],
+    salary: {
+        type: Number, 
+        //also while putting salary see here in schema it is number 
+        // so make sure you don't something like 50k , etc as that is string
+        required: true
+    },
+    experienceLevel:{
+        type:Number,
+        required:true,
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    jobType: {
+        type: String,
+        required: true
+    },
+    position: {
+        type: Number,
+        required: true
+    },
+    company: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Company',
+        required: true
+    },
+    created_by: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    applications: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Application',
+        }
+    ]
+},{timestamps:true});
+export const Job = mongoose.model("Job", jobSchema);
---
+++ frontend/src/utils/constant.js
@@ -0,0 +1,5 @@
+export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
+ //here 8000port used so make sure  backend also on same port 8000 so wrote PORT:8000 in .env file
+export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
+export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
+export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
